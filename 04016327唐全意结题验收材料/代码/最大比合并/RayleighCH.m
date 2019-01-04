6714ca741b2e0ee4c281f4da768475a4eae3dbd8
shicha1 <3030295766@qq.com>
2019-01-04 17:41:06 +0800

    结题材料

---
+++ 04016327唐全意结题验收材料/代码/最大比合并/RayleighCH.m
@@ -0,0 +1,27 @@
+%% function of the Rayleigh Fading Channel
+%**************************************************************************
+%本程序有版权，仅供教学使用
+%Author: 29/09/2013 wuguilu
+%2013.09.29 
+%**************************************************************************
+%==========================================================================
+% Description: 
+% Usage:  H = RayleighCH(sigma)
+% Inputs:
+%         sigma2: variance(σ^2)
+% Outputs:
+%         H: 1×n Fading coefficient matrix 
+% =========================================================================
+function H = RayleighCH(sigma2)
+
+mu = 0;	% average value(0)
+sigma = sqrt(sigma2); % Standard deviation(σ)
+H = normrnd(mu,sigma)+j*normrnd(mu,sigma);	
+% normrnd的格式是:normrnd(MU,SIGNA,m,n),产生参数为MU,SIGMA的正态分布随机数
+
+%% ++++++++++++++++++++++++ Another Method ++++++++++++++++++++++++
+% another method (n: bit number)
+% function H = RayleighCH(n)
+% H = randn(1,n)+j*randn(1,n);
+% randn()产生均值为0，方差 σ^2 = 1，标准差σ = 1的正态分布的随机数或矩阵的函数
+%++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++
